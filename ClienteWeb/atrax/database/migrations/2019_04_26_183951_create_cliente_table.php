5fba45bec16261e5101f89134cbac7978a63aec0
Felipe Alvarado Solano <i_felialva@hotmail.com>
2019-04-26 12:45:36 -0600

    Agregadas todas las migraciones

---
+++ ClienteWeb/atrax/database/migrations/2019_04_26_183951_create_cliente_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClienteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('correo')->unique();
+            $table->string('telefono')->nullable();
+            $table->string('direccion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente');
+    }
+}
---
+++ ClienteWeb/atrax/database/migrations/2019_04_26_184050_create_factura_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacturaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('factura', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->decimal('total');
+            $table->timestamp('fecha');
+            $table->boolean('pagado');
+            $table->unsignedInteger('cliente_id');
+            $table->unsignedInteger('tiquete_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('factura');
+    }
+}
